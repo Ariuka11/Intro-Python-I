589816588820463b15c026046cf84ca481b7cc86
Ariunbold <ariun1118@gmail.com>
2020-08-06 12:00:11 -0400

    finished until 13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+foo = open('foo.txt', 'r+')
+print(foo.read())
+foo.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +20,7 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+lag = open('bar.txt', 'w')
+print(lag.write("What is your name,\nAnd What is your height"))
+lag = open('bar.txt', 'r')
+print(lag.read())
